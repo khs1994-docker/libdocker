3452a811de8936428cc882caf8c172d34a2c4e40
khs1994 <khs1994@khs1994.com>
2018-09-06 09:33:07 +0800

    Update helpers

--- src/Docker/helpers.php
+++ src/Docker/helpers.php
@@ -9,6 +9,21 @@ if (!(function_exists('docker'))) {
     }
 }
 
-class Docker extends \Docker\Facade
+/**
+ * @method static Docker\Swarm\Config\Client  config()
+ * @method static Docker\Container\Client     container()
+ * @method static Docker\Distribution\Client  distribution()
+ * @method static Docker\Image\Client         image()
+ * @method static Docker\Network\Client       network()
+ * @method static Docker\Plugin\Client        plugin()
+ * @method static Docker\Swarm\Secret\Client  secret()
+ * @method static Docker\Swarm\Client         swarm()
+ * @method static Docker\Swarm\Node\Client    node()
+ * @method static Docker\Swarm\Service\Client service()
+ * @method static Docker\System\Client        system()
+ * @method static Docker\Task\Client          task()
+ * @method static Docker\Volume\Client        volume()
+ */
+class Docker
 {
 }
